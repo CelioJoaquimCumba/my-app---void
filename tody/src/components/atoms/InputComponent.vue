7f5d4aa74c3ed1ed343bfd3439ef8817bf03276d
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-01-31 21:29:39 +0200

    feat: add login and register page

---
+++ tody/src/components/atoms/InputComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import { defineProps } from 'vue';
+    const { label } = defineProps(['label'])
+    
+</script>
+
+<template>
+    <div class="flex flex-col items-start gap-2 self-stretch">
+        <span>{{ label }}</span>
+        <input v-bind="$attrs" class="w-full border border-gray-300 rounded-2xl px-3 py-2 focus:outline-none focus:ring-0 focus:border-green-500 text-base leading-6 font-normal">
+    </div>
+</template>
---
+++ tody/src/views/RegisterView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ButtonComponentVue from '@/components/atoms/ButtonComponent.vue';
+import InputComponent from '@/components/atoms/InputComponent.vue';
+</script>
+
+<template>
+    <main class="w-full h-screen flex items-center justify-center">
+        <div
+            class="flex w-full max-w-md py-16 px-8 flex-col justify-center items-start gap-4 rounded-sm bg-white h-full shadow-sm sm:bg-gray-50 sm:h-auto sm:border sm:border-200">
+            <h1 class="text-2xl leading-8 font-medium">Create an account</h1>
+            <span class=" text-base leading-6 font-normal">Create an account to store your tasks</span>
+            <InputComponent label="Email" placeholder="Insert your email" />
+            <InputComponent label="Password" type="password" placeholder="Insert your password" />
+            <ButtonComponentVue>
+                Create account
+            </ButtonComponentVue>
+            <div class="flex self-stretch justify-center gap-1 ">
+                <span class="text-base leading-6 font-normal">Already have an account?</span>
+                <router-link to="/login"
+                    class="text-base leading-6 font-normal underline text-green-600">Login</router-link>
+            </div>
+        </div>
+    </main>
+</template>
