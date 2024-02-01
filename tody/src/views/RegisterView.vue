063d94b5465be01da068d9006d9d3d7a3e881f5f
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-02-01 11:57:02 +0200

    fix: layout on mobile

--- tody/src/views/RegisterView.vue
+++ tody/src/views/RegisterView.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import ButtonComponentVue from '@/components/atoms/ButtonComponent.vue';
 import InputComponent from '@/components/atoms/InputComponent.vue';
+
+const handleSubmit = () => {
+    console.log("login sample")
+}
+
 </script>
 
 <template>
     <main class="w-full h-screen flex items-center justify-center">
-        <div
-            class="flex w-full max-w-md py-16 px-8 flex-col justify-center items-start gap-4 rounded-sm bg-white h-full shadow-sm  sm:h-auto sm:border sm:border-200">
+        <form
+            class="flex w-full max-w-md py-16 px-12 flex-col justify-center items-start gap-4 rounded-sm bg-white  shadow-sm  sm:h-auto sm:border sm:border-200" @submit.prevent="handleSubmit">
             <h1 class="text-2xl leading-8 font-medium">Create an account</h1>
             <span class=" text-base leading-6 font-normal">Create an account to store your tasks</span>
             <InputComponent label="Email" placeholder="Insert your email" />
             <InputComponent label="Password" type="password" placeholder="Insert your password" />
-            <ButtonComponentVue>
+            <ButtonComponentVue type="submit">
                 Create account
             </ButtonComponentVue>
             <div class="flex self-stretch justify-center gap-1 ">
@@ -19,6 +24,6 @@ import InputComponent from '@/components/atoms/InputComponent.vue';
                 <router-link to="/login"
                     class="text-base leading-6 font-normal underline text-primary">Login</router-link>
             </div>
-        </div>
+        </form>
     </main>
 </template>
