82c53628eb55cb899298f4912ffd3e637c2458dc
Célio Cumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-04-11 12:47:52 +0200

    Delete tody directory

--- tody/src/components/atoms/TaskOptionsComponent.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-    import { EllipsisHorizontalIcon } from "@heroicons/vue/24/solid";
-</script>
-
-<template>
-    <div class="flex p-2 hover:bg-gray-100  active:bg-white rounded-lg">
-        <EllipsisHorizontalIcon class="h-6 w-6" />
-    </div>
-</template>
